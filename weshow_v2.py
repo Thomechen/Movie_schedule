380b15a98045af911fd92228bf98a46a820b90b2
Austin <island6614@gmail.com>
2020-11-24 17:33:33 +0800

    fix

--- weshow_v2.py
+++ weshow_v2.py
@@ -1,16 +1,10 @@
 #%%
 import requests
-import urllib.request
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from time import sleep
-import re
 movie_list = []
 movie_link_list = []
+date_list = []
 my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69'}
 request = requests.session()
 url = 'https://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx?cinema=1|TP'
@@ -28,9 +22,18 @@ select = input('Movie select:')
 url = 'https://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx'+movie_link_list[int(select)]
 r = request.get(url,headers = my_headers)
 soup = BeautifulSoup(r.text,'html.parser')
-for t in soup.select('div.movieDay'):
+date = soup.select('div.movieDay')
+for t in date:
     print(t.text)
+    #date_list.append(t.select('h4')[0].text)
 
+'''
+select = input('Date select:')
+str = date_list[int(select)]
+time = soup.find_all('div',{'class':'movieDay'},string = str )
+for t in time:
+    print(t.select('li'))
+'''
 
 
 
