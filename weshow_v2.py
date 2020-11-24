bf732bacc1c8da2bea93e06931394ca172b7e168
Austin <island6614@gmail.com>
2020-11-24 15:24:31 +0800

    fix

--- weshow_v2.py
+++ weshow_v2.py
@@ -9,37 +9,28 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from time import sleep
 import re
+movie_list = []
+movie_link_list = []
 my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69'}
 request = requests.session()
-#套件輸入
-title_list = []
-title_link_list = []
-theater_id_list = []
-#清單建立
-i = 1
-for i in range(4):
-    url = 'https://www.vscinemas.com.tw/vsweb/film/index.aspx?p='+str(i)
-    r = request.get(url,headers = my_headers)
-    #開起網頁
-    soup = BeautifulSoup(r.text,'html.parser')
-    title = soup.select('h2 a')
-    for t in title:
-        title_list.append(t.text)
-        title_link_list.append('https://www.vscinemas.com.tw/vsweb/film/'+t.get('href')+'#')
-
-for i in range(1,len(title_list),1):
-    print(str(i),'.',title_list[i])
-    
-select = input('Select Movie:')
-print('----------------------------------------------------------------------------------')
-select_movie = title_list[int(select)]#電影名稱
-url = title_link_list[int(select)]
+url = 'https://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx?cinema=1|TP'
 r = request.get(url,headers = my_headers)
 soup = BeautifulSoup(r.text,'html.parser')
-theater_id = soup.select('li.show ul p a')
-for t in theater_id:
-    th_id = t.get('href')
-    print(th_id[1:])
+title = soup.select('ul.movieList a')
+
+for t in title:
+    movie_list.append(t.text)
+    movie_link_list.append(t.get('href'))
+for t in range(len(movie_list)):
+    print(str(t)+'.'+movie_list[t])
+
+select = input('Movie select:')
+url = 'https://www.vscinemas.com.tw/vsTicketing/ticketing/ticket.aspx'+movie_link_list[int(select)]
+r = request.get(url,headers = my_headers)
+soup = BeautifulSoup(r.text,'html.parser')
+for t in soup.select('div.movieDay'):
+    print(t.text)
+
 
 
 
